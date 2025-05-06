d8b0d3918eb252400188ae27839b5f625d603de2
Zeeshan Khan <126192035+Zeeshanprojects@users.noreply.github.com>
2025-05-06 15:33:01 +0500

    LOOKBOOK

--- PTX-2/ptx-2/src/Pages/LookBook1.jsx
+++ PTX-2/ptx-2/src/Pages/LookBook1.jsx
@@ -2,40 +2,35 @@ import { useEffect } from "react";
 import React from "react";
 
 export default function LookBook1() {
-  useEffect(()=>{
-    document.title="LookBook V1- Pakistan Textile Exchange"
-  })
+  useEffect(() => {
+    document.title = "LookBook V1 - Pakistan Textile Exchange";
+  }, []);
+
   return (
-    <>
     <div
       style={{
         width: "100vw",
         height: "100vh",
-        overflow: "hidden", // Prevents extra scrollbar
+        overflow: "hidden",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        paddingTop: "60px", // Adjust based on navbar height
-
+        paddingTop: "60px",
       }}
     >
       <object
         data="/Docs/dummy.pdf"
-        type="document/pdf"
+        type="application/pdf"
         width="100%"
         height="100%"
-        style={{
-          border: "none",
-          overflow: "hidden", 
-        }}
-      ><div className="space"></div>
+        style={{ border: "none", overflow: "hidden" }}
+      >
+        {/* Fallback message only shows if PDF cannot render */}
         <p>
-          Your browser does not support PDFs.{" "}
-          <a href="/Docs/Zeeshan-CV.pdf">Download PDF</a>
+          Your browser does not support PDFs.
+          <a href="/Docs/dummy.pdf" download> Download PDF</a>
         </p>
       </object>
     </div>
-    </>
-   
   );
 }
