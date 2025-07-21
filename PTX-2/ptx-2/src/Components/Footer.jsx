707c64b59f91833e866d1a9ea67beda317b8f68e
Zeeshan Khan <126192035+Zeeshanprojects@users.noreply.github.com>
2025-07-21 01:07:35 -0700

    Full code Updated

--- PTX-2/ptx-2/src/Components/Footer.jsx
+++ PTX-2/ptx-2/src/Components/Footer.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import Image from "../Images/Image";
 import "./Footer.style.css";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { faFacebookF, faTwitter, faInstagram, faLinkedinIn, faTiktok } from '@fortawesome/free-brands-svg-icons'
+import {
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+  faLinkedinIn,
+  faTiktok,
+} from "@fortawesome/free-brands-svg-icons";
 export default function Footer() {
   return (
     <>
-    
       <div className="container-fluid p-0">
-        
         {/* Footer */}
         <footer className="text-center text-lg-start text-dark">
           {/* Section: Social media */}
@@ -22,54 +26,130 @@ export default function Footer() {
               <div className="row mt-3">
                 {/* Grid column */}
                 <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mb-4">
-  {/* Content */}
-<img src={Image.logo} alt="logo"></img>
+                  {/* Content */}
+                  <img src={Image.logo} alt="logo"></img>
 
+                  <br />
+                  <br />
+                  <p>
+                    <Link to="/" className="text-dark">
+                      Laguna Beach, CA 92651
+                    </Link>
+                  </p>
+                  <p>
+                    <Link to="#" className="text-dark">
+                      Email: david@paktex.com
+                    </Link>
+                  </p>
+                  <p>
+                    <Link to="#" className="text-dark">
+                      Phone: +1 (949) 283-9554
+                    </Link>
+                  </p>
 
-  {/* <hr
-    className="mb-4 mt-0 d-inline-block mx-auto"
-    style={{
-      width: 60,
-      backgroundColor: "#7c4dff",
-      height: 2,
-    }}
-  /> */}
-<br/><br/>
-  <p>
-    <Link to="/" className="text-dark">
-      Laguna Beach, CA 92651
-    </Link>
-  </p>
-  <p>
-    <Link to="#" className="text-dark">
-      Email: david@paktex.com
-    </Link>
-  </p>
-  <p>
-    <Link to="#" className="text-dark">
-      Phone: +1 (949) 283-9554
-    </Link>
-  </p>
-
-  {/* Social Media Icons */}
-  <div className="social-icons mt-4">
-    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="me-2">
-    <FontAwesomeIcon icon={faFacebookF}  style={{color: 'black', border: '2px solid black', borderRadius: '50%', padding: '8px', width: '15px', height: '15px', textAlign: 'center' }} />
-    </a>
-    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="me-2">
-    <FontAwesomeIcon icon={faTwitter} style={{ fontSize: '5px', color: 'black', border: '2px solid black', borderRadius: '50%', padding: '8px', width: '15px', height: '15px', textAlign: 'center' }} />
-    </a>
-    <a href="https://tiktok.com" target="_blank" rel="noopener noreferrer" className="me-2">
-    <FontAwesomeIcon icon={faTiktok} style={{ fontSize: '5px', color: 'black', border: '2px solid black', borderRadius: '50%', padding: '8px', width: '15px', height: '15px', textAlign: 'center' }} />
-    </a>
-    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="me-2">
-    <FontAwesomeIcon icon={faInstagram} style={{ fontSize: '5px', color: 'black', border: '2px solid black', borderRadius: '50%', padding: '8px', width: '15px', height: '15px', textAlign: 'center' }} />
-    </a>
-    <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="me-2">
-    <FontAwesomeIcon icon={faLinkedinIn} style={{ fontSize: '5px', color: 'black', border: '2px solid black', borderRadius: '50%', padding: '8px', width: '15px', height: '15px', textAlign: 'center' }} />
-    </a>
-  </div>
-</div>
+                  {/* Social Media Icons */}
+                  <div className="social-icons mt-4">
+                    <a
+                      href="https://facebook.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="me-2"
+                    >
+                      <FontAwesomeIcon
+                        icon={faFacebookF}
+                        style={{
+                          color: "black",
+                          border: "2px solid black",
+                          borderRadius: "50%",
+                          padding: "8px",
+                          width: "15px",
+                          height: "15px",
+                          textAlign: "center",
+                        }}
+                      />
+                    </a>
+                    <a
+                      href="https://twitter.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="me-2"
+                    >
+                      <FontAwesomeIcon
+                        icon={faTwitter}
+                        style={{
+                          fontSize: "5px",
+                          color: "black",
+                          border: "2px solid black",
+                          borderRadius: "50%",
+                          padding: "8px",
+                          width: "15px",
+                          height: "15px",
+                          textAlign: "center",
+                        }}
+                      />
+                    </a>
+                    <a
+                      href="https://tiktok.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="me-2"
+                    >
+                      <FontAwesomeIcon
+                        icon={faTiktok}
+                        style={{
+                          fontSize: "5px",
+                          color: "black",
+                          border: "2px solid black",
+                          borderRadius: "50%",
+                          padding: "8px",
+                          width: "15px",
+                          height: "15px",
+                          textAlign: "center",
+                        }}
+                      />
+                    </a>
+                    <a
+                      href="https://instagram.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="me-2"
+                    >
+                      <FontAwesomeIcon
+                        icon={faInstagram}
+                        style={{
+                          fontSize: "5px",
+                          color: "black",
+                          border: "2px solid black",
+                          borderRadius: "50%",
+                          padding: "8px",
+                          width: "15px",
+                          height: "15px",
+                          textAlign: "center",
+                        }}
+                      />
+                    </a>
+                    <a
+                      href="https://linkedin.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="me-2"
+                    >
+                      <FontAwesomeIcon
+                        icon={faLinkedinIn}
+                        style={{
+                          fontSize: "5px",
+                          color: "black",
+                          border: "2px solid black",
+                          borderRadius: "50%",
+                          padding: "8px",
+                          width: "15px",
+                          height: "15px",
+                          textAlign: "center",
+                        }}
+                      />
+                    </a>
+                  </div>
+                </div>
                 {/* Grid column */}
                 {/* Grid column */}
                 <div className="col-md-2 col-lg-3 col-xl-3 me-auto mb-4">
@@ -88,7 +168,7 @@ export default function Footer() {
                       Home
                     </Link>
                   </p>
-                
+
                   <p>
                     <Link to="/LookBook-V1" className="text-dark">
                       LOOKBOOK
@@ -130,30 +210,30 @@ export default function Footer() {
                 </div>
 
                 <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-  {/* Links */}
-  <h1 className="text">Sign Up for Email</h1>
-  <hr
-    className="mb-4 mt-0 d-inline-block mx-auto"
-    style={{
-      width: 100,
-      backgroundColor: "#7c4dff",
-      height: 2,
-    }}
-  />
-  <p>
-    Sign up to get first dibs on new arrivals, sales, exclusive content, events and more!
-  </p>
+                  {/* Links */}
+                  <h1 className="text">Sign Up for Email</h1>
+                  <hr
+                    className="mb-4 mt-0 d-inline-block mx-auto"
+                    style={{
+                      width: 100,
+                      backgroundColor: "#7c4dff",
+                      height: 2,
+                    }}
+                  />
+                  <p>
+                    Sign up to get first dibs on new arrivals, sales, exclusive
+                    content, events and more!
+                  </p>
 
-  <div className="email-subscribe">
-    <input
-      type="email"
-      placeholder="Enter your email"
-      className="subscribe-input"
-    />
-    <button className="subscribe-btn">Subscribe</button>
-  </div>
-</div>
-
+                  <div className="email-subscribe">
+                    <input
+                      type="email"
+                      placeholder="Enter your email"
+                      className="subscribe-input"
+                    />
+                    <button className="subscribe-btn">Subscribe</button>
+                  </div>
+                </div>
               </div>
             </div>
           </section>
@@ -161,8 +241,8 @@ export default function Footer() {
             © 2025 |
             <Link className="text-dark ms-1" to="/">
               PTX
-              </Link>
-                        </div>
+            </Link>
+          </div>
         </footer>
       </div>
     </>
