380428b7916d9e6d6495edefc36f60932a782921
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-08-09 05:04:46 -0700

    code updated

--- PTX-2/ptx-2/src/Pages/LookBook1.jsx
+++ PTX-2/ptx-2/src/Pages/LookBook1.jsx
@@ -19,60 +19,59 @@ export default function LookBook1() {
 
   return (
     <>
- <br/>
-     
+      <br />
+
       <div
-      className="mt-5 p-0"
-      style={{
-        width: "100vw",
-        height: "100vh",
-        overflow: "hidden",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        paddingTop: "60px",
-        padding: "20px",
-        boxSizing: "border-box",
-        flexDirection: "column",
-        textAlign: "center",
-        
-      }}
-    >
-      {isMobile ? (
-        <>
-          <p>This document is best viewed on desktop.</p>
-          <a
-            href="/Docs/catalog.pdf"
-            download
-            style={{
-              padding: "10px 20px",
-              backgroundColor: "#000",
-              color: "#fff",
-              textDecoration: "none",
-              borderRadius: "5px",
-            }}
+        className="mt-5 p-0"
+        style={{
+          width: "100vw",
+          height: "100vh",
+          overflow: "hidden",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: "60px",
+          padding: "20px",
+          boxSizing: "border-box",
+          flexDirection: "column",
+          textAlign: "center",
+        }}
+      >
+        {isMobile ? (
+          <>
+            <p>This document is best viewed on desktop.</p>
+            <a
+              href="/Docs/catalog.pdf"
+              download
+              style={{
+                padding: "10px 20px",
+                backgroundColor: "#000",
+                color: "#fff",
+                textDecoration: "none",
+                borderRadius: "5px",
+              }}
+            >
+              Download PDF
+            </a>
+          </>
+        ) : (
+          <object
+            data="/Docs/catalog.pdf"
+            type="application/pdf"
+            width="100%"
+            height="100%"
+            style={{ border: "none" }}
           >
-            Download PDF
-          </a>
-        </>
-      ) : (
-        <object
-          data="/Docs/catalog.pdf"
-          type="application/pdf"
-          width="100%"
-          height="100%"
-          style={{ border: "none" }}
-        >
-          <p>
-            Your browser does not support PDFs.
-            <a href="/Docs/catalog.pdf" download> Download PDF</a>
-          </p>
-        </object>
-      )}
-    </div>
-  
+            <p>
+              Your browser does not support PDFs.
+              <a href="/Docs/catalog.pdf" download>
+                {" "}
+                Download PDF
+              </a>
+            </p>
+          </object>
+        )}
+      </div>
     </>
-   
-    
   );
 }
