bf6748cd8fd2b51f9f57f3b9d908e7d92d03054e
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-08-13 01:59:11 -0700

    preloader updated

--- PTX-2/ptx-2/src/App.js
+++ PTX-2/ptx-2/src/App.js
@@ -1,106 +1,106 @@
-import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
-import { useState, useEffect } from "react";
-import Header from "./Components/Header";
-import Home from "./Pages/Home";
-import Product from "./Pages/Product";
-import Signup from "./Pages/Signup";
-import ContactUs from "./Pages/Contactus";
-import PrivacyPolicy from "./Pages/PrivacyPolicy";
-import ScrollToTop from "./Components/ScrollToTop";
-import LookBook1 from "./Pages/LookBook1";
-import LookBook2 from "./Pages/LookBook2";
-import Tshirts from "./Pages/Tshirts";
-import Hoodies from "./Pages/Hoodies";
-import Login from "./Pages/Login";
-import Preloader from "./Components/Preloader";
-import Cart from "./Pages/Cart";
-import Search from "./Pages/Search";
-import ProductDetails from "./Pages/ProductDetails";
-import AboutUs from "./Pages/AboutUs";
-import Services from "./Pages/Services";
-import Manufacturing from "./Pages/Manufacturing";
-import QualityAssurance from "./Pages/QualityAssurance";
-import Logistics from "./Pages/Logistics";
-import Sourcing from "./Pages/Sourcing";
-import Studio from "./Pages/Studio";
-import Inquireform from "./Pages/Inquireform";
-import ProductDevelopment from "./Pages/ProductDevelopment";
-import Blog1 from "./Pages/Blog1";
-import Blog2 from "./Pages/Blog2";
-import Blog3 from "./Pages/Blog3";
-import Checkout from "./Pages/Checkout";
-import Teams from "./Pages/Teams";
-import Teaminfo from "./Pages/Teaminfo";
-import Gallery from "./Pages/Gallery";
-function AppContent() {
-  const [loading, setLoading] = useState(true);
-  const location = useLocation();
+  import "./App.css";
+  import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useLocation,
+  } from "react-router-dom";
+  import { useState, useEffect } from "react";
+  import Header from "./Components/Header";
+  import Home from "./Pages/Home";
+  import Product from "./Pages/Product";
+  import Signup from "./Pages/Signup";
+  import ContactUs from "./Pages/Contactus";
+  import PrivacyPolicy from "./Pages/PrivacyPolicy";
+  import ScrollToTop from "./Components/ScrollToTop";
+  import LookBook1 from "./Pages/LookBook1";
+  import LookBook2 from "./Pages/LookBook2";
+  import Tshirts from "./Pages/Tshirts";
+  import Hoodies from "./Pages/Hoodies";
+  import Login from "./Pages/Login";
+  import Preloader from "./Components/Preloader";
+  import Cart from "./Pages/Cart";
+  import Search from "./Pages/Search";
+  import ProductDetails from "./Pages/ProductDetails";
+  import AboutUs from "./Pages/AboutUs";
+  import Services from "./Pages/Services";
+  import Manufacturing from "./Pages/Manufacturing";
+  import QualityAssurance from "./Pages/QualityAssurance";
+  import Logistics from "./Pages/Logistics";
+  import Sourcing from "./Pages/Sourcing";
+  import Studio from "./Pages/Studio";
+  import Inquireform from "./Pages/Inquireform";
+  import ProductDevelopment from "./Pages/ProductDevelopment";
+  import Blog1 from "./Pages/Blog1";
+  import Blog2 from "./Pages/Blog2";
+  import Blog3 from "./Pages/Blog3";
+  import Checkout from "./Pages/Checkout";
+  import Teams from "./Pages/Teams";
+  import Teaminfo from "./Pages/Teaminfo";
+  import Gallery from "./Pages/Gallery";
+  function AppContent() {
+    const [loading, setLoading] = useState(true);
+    const location = useLocation();
 
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, [location.pathname]);
+    useEffect(() => {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 5000);
+    }, [location.pathname]);
 
-  return (
-    <div className="App">
-      {loading ? (
-        <Preloader />
-      ) : (
-        <>
-          <ScrollToTop />
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Aboutus" element={<AboutUs />} />
-            <Route path="/Services" element={<Services />} />
-            <Route path="/Manufacturing" element={<Manufacturing />} />
-            <Route path="/QualityAssurance" element={<QualityAssurance />} />
-            <Route path="/Logistics" element={<Logistics />} />
-            <Route path="/Sourcing" element={<Sourcing />} />
-            <Route path="/Studio" element={<Studio />} />
-            <Route
-              path="/productdevelopment"
-              element={<ProductDevelopment />}
-            />
-            <Route path="/team" element={<Teams />} />
-            <Route path="/team/:slug" element={<Teaminfo />} />
-            <Route path="/inquireform" element={<Inquireform />} />
-            <Route path="/Product" element={<Product />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/Contactus" element={<ContactUs />} />
-            <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-            <Route path="/LookBook-V1" element={<LookBook1 />} />
-            <Route path="/LookBook-V2" element={<LookBook2 />} />
-            <Route path="/Gallery" element={<Gallery />} />
-            <Route path="/Tshirts" element={<Tshirts />} />
-            <Route path="/Hoodies" element={<Hoodies />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Cart" element={<Cart />} />
-            <Route path="/Checkout" element={<Checkout />} />
-            <Route path="/Search" element={<Search />} />
-            <Route path="/blog1" element={<Blog1 />} />
-            <Route path="/blog2" element={<Blog2 />} />
-            <Route path="/blog3" element={<Blog3 />} />
-            <Route path="/ProductDetails/:id" element={<ProductDetails />} />
-          </Routes>
-        </>
-      )}
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        {loading ? (
+          <Preloader />
+        ) : (
+          <>
+            <ScrollToTop />
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/Aboutus" element={<AboutUs />} />
+              <Route path="/Services" element={<Services />} />
+              <Route path="/Manufacturing" element={<Manufacturing />} />
+              <Route path="/QualityAssurance" element={<QualityAssurance />} />
+              <Route path="/Logistics" element={<Logistics />} />
+              <Route path="/Sourcing" element={<Sourcing />} />
+              <Route path="/Studio" element={<Studio />} />
+              <Route
+                path="/productdevelopment"
+                element={<ProductDevelopment />}
+              />
+              <Route path="/team" element={<Teams />} />
+              <Route path="/team/:slug" element={<Teaminfo />} />
+              <Route path="/inquireform" element={<Inquireform />} />
+              <Route path="/Product" element={<Product />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/Contactus" element={<ContactUs />} />
+              <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+              <Route path="/LookBook-V1" element={<LookBook1 />} />
+              <Route path="/LookBook-V2" element={<LookBook2 />} />
+              <Route path="/Gallery" element={<Gallery />} />
+              <Route path="/Tshirts" element={<Tshirts />} />
+              <Route path="/Hoodies" element={<Hoodies />} />
+              <Route path="/Login" element={<Login />} />
+              <Route path="/Cart" element={<Cart />} />
+              <Route path="/Checkout" element={<Checkout />} />
+              <Route path="/Search" element={<Search />} />
+              <Route path="/blog1" element={<Blog1 />} />
+              <Route path="/blog2" element={<Blog2 />} />
+              <Route path="/blog3" element={<Blog3 />} />
+              <Route path="/ProductDetails/:id" element={<ProductDetails />} />
+            </Routes>
+          </>
+        )}
+      </div>
+    );
+  }
 
-export default function App() {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
-}
+  export default function App() {
+    return (
+      <Router>
+        <AppContent />
+      </Router>
+    );
+  }
